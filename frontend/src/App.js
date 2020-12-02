caed179ce5c00e95fadefa3281c83fdc1f1091cd
Muhammad-Waqas-Tahir <71683137+Muhammad-Waqas-Tahir@users.noreply.github.com>
2020-12-02 10:09:36 +0500

    first-commit

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+ 
+
+function App() {
+  return (
+    <>
+     <Header />
+    <main>
+    <h1>Welcome to Boxes Sale</h1>
+    <h2>waqas</h2>
+    </main>
+     <Footer />  
+     </>
+     
+    
+  );
+}
+
+export default App;
