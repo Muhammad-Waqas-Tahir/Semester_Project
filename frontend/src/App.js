2db61cb1e22d10c5b20569cea24290502351e654
Muhammad-Waqas-Tahir <71683137+Muhammad-Waqas-Tahir@users.noreply.github.com>
2020-12-05 12:49:51 +0500

    commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,23 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import HomeScreen from "./Screens/HomeScreen";
+import ProductScreen from "./Screens/ProductScreen";
 
 function App() {
   return (
-    <>
+    <Router>
       <Header />
       <main className="py-3">
         <Container>
-          <h1>Welcome to BoxSale</h1>
+          <Route path="/" component={HomeScreen} exact />
+          <Route path="/product/:id" component={ProductScreen} />
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 }
 
