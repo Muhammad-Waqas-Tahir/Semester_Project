dfa6e09569bc3bdb1a0cf79d60296c8e3c130193
Muhammad-Waqas-Tahir <71683137+Muhammad-Waqas-Tahir@users.noreply.github.com>
2020-12-12 20:44:05 +0500

    commit

--- backend/data/products.js
+++ backend/data/products.js
@@ -2,7 +2,7 @@ const products = [
   {
     _id: "1",
     name: "Airpods Wireless Bluetooth Headphones",
-    image: "/images/airpods.jpg",
+    image: "/images/box.jpg",
     description:
       "Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working",
     brand: "Apple",
@@ -78,5 +78,4 @@ const products = [
     numReviews: 12,
   },
 ];
-
-module.exports = products;
+export default products;
