b3c26e9a7f0315bb2b7c6b764791853f31fad663
Muhammad-Waqas-Tahir <71683137+Muhammad-Waqas-Tahir@users.noreply.github.com>
2020-12-24 11:25:47 +0500

    commit

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -64,4 +64,29 @@ const getUserProfile = asyncHandler(async (req, res) => {
     throw new Error("User No Found");
   }
 });
-export { authUser, RegisterUser, getUserProfile };
+
+// @desc     update user profile
+// @route     PUT /api/users/profile
+// @acess    private
+const updateUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.name || user.email;
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
+    const updatedUser = await user.save();
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(401);
+    throw new Error("User No Found");
+  }
+});
+export { authUser, RegisterUser, getUserProfile, updateUserProfile };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,10 +4,14 @@ import {
   authUser,
   RegisterUser,
   getUserProfile,
+  updateUserProfile,
 } from "../controllers/userController.js";
 import { protect } from "../middleware/authMiddleware.js";
 router.route("/").post(RegisterUser);
 router.post("/login", authUser);
-router.route("/profile").get(protect, getUserProfile);
+router
+  .route("/profile")
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
 
 export default router;
