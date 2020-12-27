1b2604e941bbc5e9e6cdda6423f1336b3843788d
Muhammad-Waqas-Tahir <71683137+Muhammad-Waqas-Tahir@users.noreply.github.com>
2020-12-27 15:27:48 +0500

    commit

--- backend/server.js
+++ backend/server.js
@@ -23,16 +23,14 @@ app.get("/api/config/paypal", (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 );
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname + "frontend", "build", "index.html"))
-  );
-} else {
-  app.get("/", (req, res) => {
-    res.send("My app is running");
-  });
-}
+app.get("/", (req, res) => {
+  res.send("My app is running");
+});
+
+app.use(express.static(path.join(__dirname, "/frontend/build")));
+app.get("*", (req, res) =>
+  res.sendFile(path.resolve(__dirname + "frontend", "build", "index.html"))
+);
 
 app.use(notFound);
 
